21331a1d6f945e08a3bf62abecfa74e5569d9f6e
catlair <catlair@qq.com>
2023-08-09 00:06:17 +0800

    up

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
       { async: '' },
       `setTimeout(() => {
       twikoo.init({
-        envId: 'https://bt-twikoo.vercel.app', // 腾讯云环境填 envId；Vercel 环境填地址（https://xxx.vercel.app）
+        envId: 'https://bt-twikoo.2024666.xyz', // 腾讯云环境填 envId；Vercel 环境填地址（https://xxx.vercel.app）
         el: '#tcomment', // 容器元素
         // path: location.pathname, // 用于区分不同文章的自定义 js 路径，如果您的文章路径不是 location.pathname，需传此参数
         lang: 'zh-CN', // 用于手动设定评论区语言，支持的语言列表 https://github.com/imaegoo/twikoo/blob/main/src/client/utils/i18n/index.js
