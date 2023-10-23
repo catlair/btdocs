c5635d589f8bde44bdf5cc7b2d5189d98aec9c7b
catlair <catlair@qq.com>
2023-10-23 22:19:17 +0800

    up

--- docs/.vitepress/store/config.ts
+++ docs/.vitepress/store/config.ts
@@ -68,6 +68,43 @@ const useConfigStore = defineStore('config', {
         // 购买追漫（优先级低）
         love: true,
       },
+      jury: {
+        // 重复运行次数，小于0为无限制
+        repeat: 999,
+        // 默认投票 0-3 好-无法判断，从中随机
+        vote: [0, 0, 1],
+        // 是否采用参考投票
+        opinion: true,
+        // 参考投票最少人数
+        opinionMin: 3,
+        // 排除投票 0-3 好-无法判断，用于配合参考投票，不影响【默认投票】配置
+        notOpinion: [3],
+        // 没有案件后的等待时间（分）
+        waitTime: 20,
+        // insiders 参考值
+        insiderWeight: 0.8,
+        // 是否观看视频 0 不观看，1 观看
+        insiders: [0, 1],
+        // 是否匿名 0 不匿名，1 匿名
+        anonymous: [0, 1],
+        // 云函数下使用新的触发器进行休眠
+        newTrigger: true,
+        // 异步，非云函数下使用。不支持推送结果
+        async: false,
+      },
+      gift: {
+        /** 自定义投喂礼物用户列表 */
+        mids: [] as number[],
+        // 投喂礼物 id
+        // 辣条 小心心 能量石头 PK票 小海浪
+        id: [1, 30607, 30426, 31531, 31674],
+        // 投喂礼物 name
+        name: [] as string[],
+        // 无视其它礼物配置，投喂所有即将过期礼物
+        all: false,
+        // 仅投喂即将过期的礼物
+        expire: true,
+      },
     };
   },
   getters: {},
