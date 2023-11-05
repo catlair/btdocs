9b89f5e567bec6778658a339a042b9a0251836af
catlair <catlair@qq.com>
2023-11-05 20:17:44 +0800

    fix

--- docs/.vitepress/components/widgets/DynamicInput.tsx
+++ docs/.vitepress/components/widgets/DynamicInput.tsx
@@ -1,5 +1,5 @@
 import { defineComponent, markRaw } from 'vue';
-import vue3Utils from '@lljj/vjsf-utils/vue3Utils';
+import * as vue3Utils from '@lljj/vjsf-utils/vue3Utils';
 import * as naiveui from 'naive-ui';
 
 const { NDynamicInput } = naiveui;
